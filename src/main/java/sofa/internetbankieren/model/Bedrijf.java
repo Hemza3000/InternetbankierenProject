9426d465321d5369d27534ec91ab195e59d877cf
W.L. Tjerkstra <wichert.tjerkstra@hva.nl>
2020-12-07 17:30:26 +0100

    in mamp model gestopt

---
+++ src/main/java/sofa/internetbankieren/model/Bedrijf.java
@@ -0,0 +1,34 @@
+package sofa.internetbankieren.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bedrijf extends Klant {
+
+    private String bedrijfsnaam;
+    private int KVKNummer;
+    private String sector;
+    private String BTWNummer;
+    private Medewerker accountmanager;
+
+    //moet dit niet bedrijfsrekeningen zijn?
+    private List<Bedrijfsrekening> rekeningen = new ArrayList<>();
+
+    public Bedrijf(int idKlant, String straatnaam, String huisnummer, String postcode, String woonplaats, String bedrijfsnaam,
+                   int KVKNummer, String sector, String BTWNummer, Medewerker accountmanager, List<Bedrijfsrekening> rekeningen) {
+        super(idKlant, straatnaam, huisnummer, postcode, woonplaats);
+        this.bedrijfsnaam = bedrijfsnaam;
+        this.KVKNummer = KVKNummer;
+        this.sector = sector;
+        this.BTWNummer = BTWNummer;
+        this.accountmanager = accountmanager;
+        this.rekeningen = rekeningen;
+    }
+    public Bedrijf(){
+
+    }
+    //Vraag: moet deze er nog bij?
+    public void addBedrijf(){
+
+    }
+}
---
+++ src/main/java/sofa/internetbankieren/model/Particulier.java
@@ -0,0 +1,105 @@
+package sofa.internetbankieren.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Particulier extends Klant {
+
+    private String voornaam;
+    private String tussenvoegsels;
+    private String achternaam;
+    private Date geboortedatum;
+    private int BSN;
+
+    private List<Priverekening> priverekeningen = new ArrayList<>();
+    private List<Bedrijfsrekening> bedrijfsrekeningen = new ArrayList<>();
+
+
+    public Particulier(int idKlant, String straatnaam, String huisnummer, String postcode, String woonplaats, String voornaam, String tussenvoegsels, String achternaam, Date geboortedatum, int BSN) {
+        super(idKlant, straatnaam, huisnummer, postcode, woonplaats);
+        this.voornaam = voornaam;
+        this.tussenvoegsels = tussenvoegsels;
+        this.achternaam = achternaam;
+        this.geboortedatum = geboortedatum;
+        this.BSN = BSN;
+    }
+
+    public Particulier() {
+            }
+
+
+
+    public Particulier(int idKlant, String straatnaam, String huisnummer, String postcode, String woonplaats, String voornaam,
+                       String tussenvoegsels, String achternaam, Date geboortedatum,
+                       int BSN, List<Priverekening> priverekeningen, List<Bedrijfsrekening> bedrijfsrekeningen) {
+        super(idKlant, straatnaam, huisnummer, postcode, woonplaats);
+        this.voornaam = voornaam;
+        this.tussenvoegsels = tussenvoegsels;
+        this.achternaam = achternaam;
+        this.geboortedatum = geboortedatum;
+        this.BSN = BSN;
+        this.priverekeningen = priverekeningen;
+        this.bedrijfsrekeningen = bedrijfsrekeningen;
+    }
+    public void addParticulier(){
+
+    }
+
+    //Begin getters/setters
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getTussenvoegsels() {
+        return tussenvoegsels;
+    }
+
+    public void setTussenvoegsels(String tussenvoegsels) {
+        this.tussenvoegsels = tussenvoegsels;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public Date getGeboortedatum() {
+        return geboortedatum;
+    }
+
+    public void setGeboortedatum(Date geboortedatum) {
+        this.geboortedatum = geboortedatum;
+    }
+
+    public int getBSN() {
+        return BSN;
+    }
+
+    public void setBSN(int BSN) {
+        this.BSN = BSN;
+    }
+
+    public List<Priverekening> getPriverekeningen() {
+        return priverekeningen;
+    }
+
+    public void setPriverekeningen(List<Priverekening> priverekeningen) {
+        this.priverekeningen = priverekeningen;
+    }
+
+    public List<Bedrijfsrekening> getBedrijfsrekeningen() {
+        return bedrijfsrekeningen;
+    }
+
+    public void setBedrijfsrekeningen(List<Bedrijfsrekening> bedrijfsrekeningen) {
+        this.bedrijfsrekeningen = bedrijfsrekeningen;
+    }
+}
