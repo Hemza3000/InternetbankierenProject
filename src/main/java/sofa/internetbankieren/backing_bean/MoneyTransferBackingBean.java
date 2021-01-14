c88ab730a9fa1a28f0bdf196dfa732317bec02e8
W.L. Tjerkstra <wichert.tjerkstra@hva.nl>
2021-01-14 09:29:36 +0100

    commit MTBB

--- src/main/java/sofa/internetbankieren/backing_bean/MoneyTransferBackingBean.java
+++ src/main/java/sofa/internetbankieren/backing_bean/MoneyTransferBackingBean.java
@@ -1,51 +1,28 @@
 package sofa.internetbankieren.backing_bean;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class MoneyTransferBackingBean {
-
-    private Logger logger = LoggerFactory.getLogger(MoneyTransferBackingBean.class);
-
-    private String IBAN;
     private double bedrag;
+    private String tegenrekening;
     private String omschrijving;
-
-    public MoneyTransferBackingBean(String IBAN, double bedrag, String omschrijving) {
-        super();
-        this.IBAN = IBAN;
+    public MoneyTransferBackingBean(double bedrag, String tegenrekening, String omschrijving) {
         this.bedrag = bedrag;
+        this.tegenrekening = tegenrekening;
         this.omschrijving = omschrijving;
     }
-
-    public Logger getLogger() {
-        return logger;
-    }
-
-    public void setLogger(Logger logger) {
-        this.logger = logger;
-    }
-
-    public String getIBAN() {
-        return IBAN;
-    }
-
-    public void setIBAN(String IBAN) {
-        this.IBAN = IBAN;
-    }
-
     public double getBedrag() {
         return bedrag;
     }
-
     public void setBedrag(double bedrag) {
         this.bedrag = bedrag;
     }
-
+    public String getTegenrekening() {
+        return tegenrekening;
+    }
+    public void setTegenrekening(String tegenrekening) {
+        this.tegenrekening = tegenrekening;
+    }
     public String getOmschrijving() {
         return omschrijving;
     }
-
     public void setOmschrijving(String omschrijving) {
         this.omschrijving = omschrijving;
     }
