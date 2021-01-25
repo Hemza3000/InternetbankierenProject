83a6582e025109358f449e0dc56b970bc65118b4
Hemza Lasri <h.lasri@hva.nl>
2021-01-25 18:24:31 +0100

    Verdere verbeteringen bootstrap pagina's registratie particulier

--- src/main/java/sofa/internetbankieren/controller/RegistrationController.java
+++ src/main/java/sofa/internetbankieren/controller/RegistrationController.java
@@ -1,174 +1,103 @@
 package sofa.internetbankieren.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import sofa.internetbankieren.backing_bean.LoginFormBackingBean;
-import sofa.internetbankieren.backing_bean.RegisterFormPartBackingBean;
 import sofa.internetbankieren.model.*;
 import sofa.internetbankieren.repository.*;
-import sofa.internetbankieren.service.AccountService;
-import sofa.internetbankieren.service.RegisterService;
+import sofa.internetbankieren.service.CustomerService;
 
 /**
  * @author Wichert Tjerkstra (particulier), Wendy Ellens (bedrijf)
- * aangemaakt op 9 dec
  *
  * Handelt de registratie van (particuliere/zakelijke) klanten af in 4 stappen:
- * 1. Keuze particulier/bedrijf
- * 2. Invoer klantgegevens
- * 3. Eventuele wijzigingen klantgegevens na controle door klant
- * 4. Invoer logingegevens
+ * 1. Ophalen registratiepagina
+ * 2. Verwerken ingevoerde klantgegevens
+ * 3. Verwerken eventuele wijzigingen klantgegevens na controle door klant
+ * 4. Verwerken ingevoerde logingegevens
  */
-@SessionAttributes({"klant", "ingelogde"})
+@SessionAttributes("klant")
 @Controller
 public class RegistrationController {
 
     // Zoals aangegeven door de PO, is het hoofd MKB (medewerker 2) altijd de accountmanager.
     public final static int ID_ACCOUNTMANAGER = 2;
-    private final ParticulierDAO particulierDAO;
-    private final BedrijfDAO bedrijfDAO;
     private final MedewerkerDAO medewerkerDAO;
     private final BedrijfsrekeningDAO bedrijfsrekeningDAO;
     private final PriverekeningDAO priverekeningDAO;
-    private final RegisterService registerService;
-    private final AccountService accountService;
+    private final CustomerService customerService;
 
-    public RegistrationController(ParticulierDAO particulierDAO, BedrijfDAO bedrijfDAO, MedewerkerDAO medewerkerDAO,
-                                  BedrijfsrekeningDAO bedrijfsrekeningDAO, PriverekeningDAO priverekeningDAO,
-                                  RegisterService registerService, AccountService accountService) {
+    public RegistrationController(MedewerkerDAO medewerkerDAO, BedrijfsrekeningDAO bedrijfsrekeningDAO,
+                                  PriverekeningDAO priverekeningDAO, CustomerService customerService) {
         super();
-        this.particulierDAO = particulierDAO;
-        this.bedrijfDAO = bedrijfDAO;
         this.medewerkerDAO = medewerkerDAO;
         this.bedrijfsrekeningDAO = bedrijfsrekeningDAO;
         this.priverekeningDAO = priverekeningDAO;
-        this.registerService = registerService;
-        this.accountService = accountService;
+        this.customerService = customerService;
     }
 
-    @GetMapping("/register")
-    public String registerHandler() {
-        return "register/particulierOfBedrijf";
-    }
-
-    // Stap 1: keuze voor doorverwijzen naar zakelijke of particuliere registratiepagina
-    @PostMapping("/register_Zakelijk_Particulier")
-    public String choiceHandler(@RequestParam(name = "zakelijkOfParticulier") int value, Model model) {
-        if (value == 0) {
-            model.addAttribute("backingBean", new RegisterFormPartBackingBean());
-            model.addAttribute("BsnExists", false);
-            return "register/particulier";
-        } else if (value == 1) {
-            model.addAttribute("klant", new Bedrijf());
-            return "register/bedrijf";
-        }
-        return null;
-    }
-
-    // voor bootstrap
+    // Stap 1 particulier: ophalen van registratiepagina
     @GetMapping("/particulier")
-    public String particulierHandler(Model model){
-        model.addAttribute("backingBean", new RegisterFormPartBackingBean());
-        model.addAttribute("BsnExists", false);
+    public String getParticulierForm(Model model){
+        model.addAttribute("klant", new Particulier(bedrijfsrekeningDAO, priverekeningDAO));
+        model.addAttribute("fieldExists", false);
         return "register/particulier";
     }
 
-    // voor bootstrap
+    // Stap 1 bedrijf: ophalen van registratiepagina
     @GetMapping("/zakelijk")
-    public String zakelijkHandler(Model model){
-        model.addAttribute("klant", new Bedrijf());
+    public String getZakelijkForm(Model model){
+        model.addAttribute("klant", new Bedrijf(bedrijfsrekeningDAO, medewerkerDAO.getOneByID(ID_ACCOUNTMANAGER)));
         return "register/bedrijf";
     }
 
-
-    // Registratie particulier
-
     // Stap 2: verwerken ingevoerde klantgegevens
-    @PostMapping("/register_particulier")
-    public String newParticulierHandler(Model model, @ModelAttribute(name = "backingBean") RegisterFormPartBackingBean dummy) {
-        model.addAttribute("backingBean", dummy);
-        // toegevoegd door Wendy
-        // validatie voor unieke BSN
-        if (accountService.doesBsnExist(dummy.getBSN())) {
-            model.addAttribute("BsnExists", true);
-            return "register/particulier";
+    @PostMapping("/register")
+    public String postNewClientForm(@ModelAttribute("klant") Klant klant, Model model) {
+        model.addAttribute("klant", klant);
+        if (klant instanceof Bedrijf) {
+            return "register/confirmationBedrijf";
         }
-        model.addAttribute("BsnExists", false);
-        return "register/confirmationParticulier";
-    }
-
-    // Stap 3: verwerken eventuele wijzigingen in klantgegevens na controle door klant
-    @PostMapping("/registerLogin")
-    public String confirmHandler(@ModelAttribute RegisterFormPartBackingBean backingBean, Model model) {
-        // toegevoegd door Wendy
-        // validatie voor unieke BSN
-        if (accountService.doesBsnExist(backingBean.getBSN())) {
-            model.addAttribute("backingBean", backingBean);
-            model.addAttribute("BsnExists", true);
+        else {
+            if(doesBsnExist(((Particulier) klant), model)) {
+                return  "register/particulier";
+            }
             return "register/confirmationParticulier";
         }
-        Particulier p = new Particulier(backingBean, bedrijfsrekeningDAO, priverekeningDAO);
-        model.addAttribute("klant", p);
-        LoginFormBackingBean usernameForm = new LoginFormBackingBean("","");
-        model.addAttribute("doesExist", false);
-        model.addAttribute("usernameForm", usernameForm);
-        return "register/registerLogin";
-    }
-
-    // Stap 4: verwerken ingevoerde logingegevens
-    @PostMapping("/usernameForm")
-    public String confirm(Model model, @ModelAttribute LoginFormBackingBean usernameForm){
-        Klant klant = (Klant) model.getAttribute("klant");
-        // validatie voor unieke gebruikersnaam
-        if (!registerService.checkUniqueUsername(usernameForm.getUserName())) {
-            model.addAttribute("usernameForm", usernameForm);
-            model.addAttribute("doesExist", true);
-            return "register/registerLogin";
-        }
-        klant.setGebruikersnaam(usernameForm.getUserName());
-        klant.setWachtwoord(usernameForm.getPassword());
-        if (klant instanceof Particulier) {
-            particulierDAO.storeOne((Particulier) klant);
-        } else
-            bedrijfDAO.storeOne((Bedrijf) klant);
-        model.addAttribute("ingelogde", klant);
-        return "register/completed";
-    }
-
-
-    // Registratie bedrijf
-
-    // Stap 2: verwerken ingevoerde klantgegevens
-    @PostMapping("/register_zakelijk")
-    public String newBedrijfsHandler(@ModelAttribute("klant") Bedrijf bedrijf, Model model) {
-        model.addAttribute("klant", bedrijf);
-        return "register/confirmationBedrijf";
     }
 
     // Stap 3: verwerken eventuele wijzigingen in klantgegevens na controle door klant
-    @PostMapping("/confirmBedrijf")
-    public String confirmBedrijfHandler(@ModelAttribute("klant") Bedrijf bedrijf, Model model) {
-        model.addAttribute("klant", bedrijf);
-        model.addAttribute("doesExist", false);
+    @PostMapping("/confirm")
+    public String confirmNewClient(@ModelAttribute("klant") Klant klant, Model model) {
+        model.addAttribute("klant", klant);
+        if (klant instanceof Particulier && doesBsnExist(((Particulier) klant), model)) {
+            return  "register/confirmationParticulier";
+        }
         return "register/registerLogin";
     }
 
     // Stap 4: verwerken ingevoerde logingegevens
     @PostMapping("/storeLogin")
-    public String confirm(@ModelAttribute("klant") Bedrijf bedrijf, Model model) {
+    public String confirmLogin(@ModelAttribute("klant") Klant klant, Model model){
 
-        // validatie voor unieke gebruikersnaam
-        if (!registerService.checkUniqueUsername(bedrijf.getGebruikersnaam())) {
-            model.addAttribute("klant", bedrijf);
-            model.addAttribute("doesExist", true);
+        // Controleert of er in de database al klant met deze gebruikersnaam is
+        if (!customerService.checkUniqueUsername(klant.getGebruikersnaam())) {
+            model.addAttribute("klant", klant);
+            model.addAttribute("fieldExists", true);
             return "register/registerLogin";
         }
 
-        bedrijf.setAccountmanager(medewerkerDAO.getOneByID(ID_ACCOUNTMANAGER));
-        bedrijf.setBedrijfsrekeningDAO(bedrijfsrekeningDAO);
-        bedrijfDAO.storeOne(bedrijf);
-        model.addAttribute("ingelogde", bedrijf);
+        customerService.storeNewCustomer(klant);
+        model.addAttribute("klant", klant);
         return "register/completed";
+    }
+
+    // Controleert of er in de database al een particuliere klant met dit BSN is
+    private boolean doesBsnExist(Particulier particulier, Model model) {
+        if (customerService.doesBsnExist(particulier.getBSN())) {
+            model.addAttribute("fieldExists", true);
+            return true;
+        }
+        model.addAttribute("fieldExists", false);
+        return false;
     }
 }
